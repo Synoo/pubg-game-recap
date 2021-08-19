3bb01467c54d5a6ba99250518b093f0bafb06dad
Synoo <patrick.buijs33@gmail.com>
2021-08-19 20:51:29 +0200

    Add Firebase, put/get pubg api data in firestore
    Their are some issues with CORS but will be fixed in future commit
    with firebase functions.

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -1,77 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import firebase from "../firebase";
 
 const Matches = () => {
-  const [match, setMatch] = useState("");
-  const [kills, setKills] = useState("");
-  const [assists, setAssists] = useState("");
-  const [damage, setDamage] = useState("");
-  const [rosterNames, setRosterNames] = useState([]);
-  const [timeSurvived, setTimeSurvived] = useState("");
-  const [winPlace, setWinPlace] = useState("");
+  const [playerData, setPlayerData] = useState();
   const { playerName } = useParams();
-
-  const headers = {
-    Accept: "application/vnd.api+json",
-    Authorization: process.env.REACT_APP_API_KEY,
-  };
 
   useEffect(() => {
     const fetchData = async () => {
-      setRosterNames([]);
-      const result = await axios(
-        `https://api.pubg.com/shards/steam/players?filter[playerNames]=${playerName}`,
-        { headers }
-      );
-
-      const match = await axios(
-        "https://api.pubg.com/shards/steam/matches/" +
-          result.data.data[0].relationships.matches.data[0].id,
-        { headers }
-      );
-
-      let rosterId = "";
-
-      await match.data.included.map((z) => {
-        if (z.attributes.stats) {
-          if (z.attributes.stats.playerId === result.data.data[0].id) {
-            setKills(z.attributes.stats.kills);
-            setAssists(z.attributes.stats.assists);
-            setDamage(z.attributes.stats.damageDealt);
-            setTimeSurvived(z.attributes.stats.timeSurvived);
-            setWinPlace(z.attributes.stats.winPlace);
-            rosterId = z.id;
-          }
-        }
-      });
-
-      let roster = "";
-
-      await match.data.included.map((x) => {
-        if (x.relationships) {
-          x.relationships.participants.data.map((y) => {
-            if (rosterId === y.id) {
-              roster = x;
+      if (playerName) {
+        const db = firebase.firestore();
+        const docRef = db.collection("players").doc(playerName);
+        docRef
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              setPlayerData(doc.data());
+            } else {
+              console.log(`No document with name: ${playerName}`);
             }
+          })
+          .catch((error) => {
+            console.log("Error getting document:", error);
           });
-        }
-      });
-
-      await roster.relationships.participants.data.map((a) => {
-        if (rosterId !== a.id) {
-          match.data.included.map((z) => {
-            if (z.id === a.id) {
-              setRosterNames((rosterNames) => [
-                ...rosterNames,
-                z.attributes.stats.name,
-              ]);
-            }
-          });
-        }
-      });
-
-      setMatch(match.data);
+      }
     };
     if (playerName) {
       fetchData();
@@ -80,52 +32,32 @@ const Matches = () => {
 
   return (
     <div className="bg-gray-800 min-h-screen p-5">
-      {playerName && match.data ? (
+      {playerName && playerData ? (
         <>
           <div className="bg-gray-900 text-green-300 p-5 rounded-xl mb-5 flex justify-between">
             <div>
-              <p>Date: {match.data.attributes.createdAt}</p>
-              <p>Survival Time: {timeSurvived / 60}</p>
-              <p>Placement: {winPlace}</p>
-              <p>Map: {match.data.attributes.mapName}</p>
+              <p>Date: {playerData.matches[0].createdAt}</p>
+              <p>Survival Time: {playerData.matches[0].timeSurvived}</p>
+              <p>Match Type: {playerData.matches[0].matchType}</p>
+              <p>Map: {playerData.matches[0].mapName}</p>
             </div>
             <div>
-              <p>Kills: {kills}</p>
-              <p>Assists: {assists}</p>
-              <p>Damage: {damage}</p>
+              <p>Placement: {playerData.matches[0].winPlace}</p>
+              <p>Kills: {playerData.matches[0].kills}</p>
+              <p>Assists: {playerData.matches[0].assists}</p>
+              <p>Damage: {playerData.matches[0].damage}</p>
             </div>
             <div className="flex flex-col">
               <p className="font-bold">Roster</p>
               <p>{playerName}</p>
-              {rosterNames.map((r) => {
-                return <p key={r.name}>{r}</p>;
-              })}
-            </div>
-          </div>
-
-          <div className="bg-gray-900 text-green-300 p-5 rounded-xl mb-5 flex justify-between">
-            <div>
-              <p>Date: {match.data.attributes.createdAt}</p>
-              <p>Survival Time: {timeSurvived / 60}</p>
-              <p>Placement: {winPlace}</p>
-              <p>Map: {match.data.attributes.mapName}</p>
-            </div>
-            <div>
-              <p>Kills: {kills}</p>
-              <p>Assists: {assists}</p>
-              <p>Damage: {damage}</p>
-            </div>
-            <div className="flex flex-col">
-              <p className="font-bold">Roster</p>
-              <p>{name}</p>
-              {rosterNames.map((r) => {
+              {playerData.matches[0].rosterNames.map((r) => {
                 return <p key={r.name}>{r}</p>;
               })}
             </div>
           </div>
         </>
       ) : (
-        <p className="text-green-300">Enter PUBG Username in the search bar</p>
+        <p className="text-green-300">Enter PUBG username in the search bar</p>
       )}
     </div>
   );
