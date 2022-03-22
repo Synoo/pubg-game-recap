500658725110f5cdd378fff323ca88495e971e21
Synoo <patrick.buijs33@gmail.com>
2022-03-22 17:48:59 +0100

    Add nextjs-progressbar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress />
       <Header />
       <Component {...pageProps} />
     </>
